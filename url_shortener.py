2d18ab6d93446f83dd903c48e7a9fb3a2d7d3e5b
Anagha N <93267192+Anagha-N-19@users.noreply.github.com>
2022-12-14 12:35:13 +0530

    Update url_shortener.py

--- url_shortener.py
+++ url_shortener.py
@@ -3,6 +3,6 @@ import pyshorteners
 current_link = input("Enter link: ")
 s = pyshorteners.Shortener()
 short_link = s.tinyurl.short(current_link)
-print(short_link)
+print("Shortened Link:",short_link)
 
 # print(s.tinyurl.expand(short_link)) for expanded link
