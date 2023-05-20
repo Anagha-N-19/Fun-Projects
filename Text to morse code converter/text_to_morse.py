77015b17663514ebc9d6266fa5e5ba3074e80a16
Anagha N <93267192+Anagha-N-19@users.noreply.github.com>
2023-05-20 17:33:45 +0530

    Add files via upload

---
+++ Text to morse code converter/text_to_morse.py
@@ -0,0 +1,22 @@
+text = input("Enter the text here: ")
+morse_code = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.',
+    'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.',
+    'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-',
+    'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..', '9': '----.'
+}
+text = text.upper()
+morse = []
+for i in text:
+    if i == ' ':
+        morse.append(" ")
+    elif i not in morse_code.keys():
+        print("Failed")
+        break
+    else:
+        morse.append(morse_code[i])
+
+result = ''.join(morse)
+print(result)
